c875b59880d74057d13516325774ee3cf66ab510
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-06-19 15:50:04 -0500

    added if/else statement shell  for criteria

--- script.js
+++ script.js
@@ -2,22 +2,45 @@
 //Start with informing the user to select the criteria needed for a generated password.
 window.alert("Select the criteria you would like for your generated password.");
 
-//Define the constiables for each prompt
-const capitalLetters = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
+//Define the constants for each prompt
+
+const uppercaseLetters = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
 const lowercaseLetters = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
 const numbers = [0,1,2,3,4,5,6,7,8,9]
-const specialCharacters = [~,!,@,#,$,%,^,&,*,(,),_,+]
+//const specialCharacters = [~,!,@,#,$,%,^,&,*,(,),_,+]
 
 //Prompted criteria will be used in if/else statements.
-window.prompt();
-if () {
+const uppercasePrompt = window.confirm("Would you like to use upper case letters?");
+if (confirm) {
 
 }
-else{
-  
+else{ 
+
 };
 
+const lowercasePrompt = window.confirm("Would you like to use lower case letters?");
+if (confirm) {
 
+}
+else{ 
+
+};
+
+const numbersPrompt = window.confirm("Would you like to use numbers?");
+if (confirm) {
+
+}
+else{ 
+
+};
+
+const specialPrompt = window.confirm("Would you like to use special characters?");
+if (confirm) {
+
+}
+else{ 
+
+};
 
 
 
