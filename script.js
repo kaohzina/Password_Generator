464be750306d2127b21d84a2684f5eec2c4acbe0
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-06-20 14:04:56 -0500

    insert criteria prompts into main function

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-var requestedCriteria = {
-lower: getLowerLetter,
-upper: getUpperLetter,
-number: getNumber,
-symbol: getSymbol
-};
-
-
 var Letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var Numbers = [0,1,2,3,4,5,6,7,8,9]
 var specialCharacters = ["~","!","@","#","$","%","^","&","*","(",")","_","+"]
@@ -16,48 +8,61 @@ function getLowerLetter (){return Letters[Math.floor(Math.random() * Letters.len
 function getUpperLetter (){return capitalLetters[Math.floor(Math.random() * capitalLetters.length)];}
 function getNumber (){return Numbers[Math.floor(Math.random() * Numbers.length)];}
 function getSymbol (){return specialCharacters[Math.floor(Math.random() * specialCharacters.length)];}
+function getLength (){return lengthCase}
+
+function passwordCriteria(){
+
+  var lengthCase = window.prompt("How many characters do you want in your password? Choose from 8-128 characters.")
+  if (lengthCase < 8 || lengthCase > 128){ window.alert("Your generated password needs to have a length of 8-128 characters.");
+  }
+  else { var lengthcase = length
+
+  }
+  
+  var upperCase = window.confirm("Do you want uppercase letters in your generated password?");
+  if (upperCase === true) { var upper = true;
+  }
+  else { var upper = false;}
+  //
+  var lowerCase = window.confirm("Do you want lowercase letters in your generated password?");
+  if (lowerCase === true) { var lower = true;
+  }
+  else { var lower = false;
+  }
+  //
+  var numberCase = window.confirm("Do you want numbers in your generated password?");
+  if (numberCase === true) { 
+  }
+  else { 
+  }
+  //
+  var symbolCase = window.confirm("Do you want special letters in your generated password?");
+  if (symbolCase === true) {
+  }
+  else {
+  }
+
+  // lowercaseStyle();
+  // symbolStyle();
+  var requestedCriteria = {
+    length: getLength,
+    lower: lowerCase,
+    upper: upperCase,
+    number: numberCase,
+    symbol: symbolCase
+    };
+    
+  return requestedCriteria;
+  }
+  passwordCriteria();
+
+  function test(){
+  var criteria = passwordCriteria()
+  console.log(criteria)
+}
+console.log(test());
 
 
-
-var lengthCase = window.prompt("How many characters do you want in your password? Choose from 8-128 characters.")
-if (lengthCase < 8 || lengthCase > 128){ window.alert("Your generated password needs to have a length of 8-128 characters.");
-}
-else {
-
-}
-var uppercaseStyle = function() {
-    var upperCase = window.confirm("Do you want uppercase letters in your generated password?");
-    if (upperCase === true) { var upperCase
-  }
-    else {
-  }
-}
-//uppercaseStyle();
-    var lowercaseStyle = function() {
-    var lowerCase = window.confirm("Do you want lowercase letters in your generated password?");
-    if (lowerCase === true) {
-  }
-    else {
-  }
-}
-//lowercaseStyle();
-    var numberStyle = function() {
-    var numberCase = window.confirm("Do you want numbers in your generated password?");
-    if (numberCase === true) {
-    }
-    else {
-   
-  }
-}
-//numberStyle();
-    var symbolStyle = function() {
-    var symbolCase = window.confirm("Do you want special letters in your generated password?");
-    if (symbolCase === true) {
-    }
-    else {
-  }
-}
-//specialStyle();
 // Get references to the #generate element
 const generateBtn = document.querySelector("#generate");
 
@@ -71,5 +76,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
- 
+
 
