37bda8c37bb24b781306fd5d5d8a665d4826b0f4
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-06-20 08:32:43 -0500

    update style and added function to receive random style

--- script.js
+++ script.js
@@ -1,60 +1,71 @@
 // Assignment code here
-//Start with informing the user to select the criteria needed for a generated password.
-window.alert("Select the criteria you would like for your generated password.");
 
 //Define the constants for each prompt
+var Letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var Numbers = [0,1,2,3,4,5,6,7,8,9]
+var specialCharacters = ["~","!","@","#","$","%","^","&","*","(",")","_","+"]
+var capitalLetters = Letters.map(function(x){return x.toUpperCase();});
+//console.log(capitalLetters);
+function getLowerLetter (){return Letters[Math.floor(Math.random() * Letters.length)];}
+function getUpperLetter (){return capitalLetters[Math.floor(Math.random() * capitalLetters.length)];}
+function getNumber (){return Numbers[Math.floor(Math.random() * Numbers.length)];}
+function getSymbol (){return specialCharacters[Math.floor(Math.random() * specialCharacters.length)];}
 
-//const uppercaseLetters = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
-//const lowercaseLetters = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
-//const numbers = [0,1,2,3,4,5,6,7,8,9]
-//const specialCharacters = [~,!,@,#,$,%,^,&,*,(,),_,+]
 
 
-    var uppercaseStyle = function() {
+console.log(getLowerLetter());
+console.log(getUpperLetter());
+console.log(getNumber());
+console.log(getSymbol());
+
+
+
+//console.log(Math.random()*26);
+//var characterCount = window.prompt("How many characters do you want in your password? Choose from 8-128 characters.")
+
+//Start with informing the user to select the criteria needed for a generated password.
+//window.alert("Select the criteria you would like for your generated password.");
+
+
+var uppercaseStyle = function() {
     var upperCase = window.confirm("Do you want uppercase letters in your generated password?");
     if (upperCase === true) {
-    window.alert('You have chosen to have uppercase letters in your generated password!')
   }
     else {
-    window.alert('You have chosen NOT to have uppercase letters in your generated password!')
   }
 }
-uppercaseStyle();
+//uppercaseStyle();
 
     var lowercaseStyle = function() {
     var lowerCase = window.confirm("Do you want lowercase letters in your generated password?");
     if (lowerCase === true) {
-    window.alert('You have chosen to have lowercase letters in your generated password!')
   }
     else {
-    window.alert('You have chosen NOT to have lowercase letters in your generated password!')
   }
 }
-lowercaseStyle();
+//lowercaseStyle();
 
     var numberStyle = function() {
     var numberCase = window.confirm("Do you want numbers in your generated password?");
     if (numberCase === true) {
-    window.alert('You have chosen to have numbers in your generated password!')
-  }
+    }
     else {
-    window.alert('You have chosen NOT to have numbers in your generated password!')
+   
   }
 }
-numberStyle();
+//numberStyle();
 
     var specialStyle = function() {
     var specialCase = window.confirm("Do you want special letters in your generated password?");
     if (specialCase === true) {
-    window.alert('You have chosen to have special letters in your generated password!')
-  }
+    }
     else {
-    window.alert('You have chosen NOT to have special letters in your generated password!')
+   
   }
 }
-specialStyle();
+//specialStyle();
 
-
+//for (var i =0; i < 10; i++) {var num = Math.floor(Math.random() * 10) + 8;console.log(num);}
 
 
 
