c452e0ca400b7e0772f4229673aba719e52cd65a
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-08-17 10:57:09 -0500

    trouble shooting the password concat

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+//defining the values in the array lists. 
+const array1 = []
+const newArray = [""]
+// 0 to 25 in the array 
 const Letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+// 0 to 10 in the array
 const Numbers = [0,1,2,3,4,5,6,7,8,9]
+// 0 to 12 in the array 
 const specialCharacters = ["~","!","@","#","$","%","^","&","*","(",")","_","+"]
+// capital letters are the same amount as the lowercase letters. 
 const capitalLetters = Letters.map(function(x){return x.toUpperCase();});
 
 //functions to receive requested criteria
@@ -8,40 +15,41 @@ function getLowerLetter (){return Letters[Math.floor(Math.random() * Letters.len
 function getUpperLetter (){return capitalLetters[Math.floor(Math.random() * capitalLetters.length)];}
 function getNumber (){return Numbers[Math.floor(Math.random() * Numbers.length)];}
 function getSymbol (){return specialCharacters[Math.floor(Math.random() * specialCharacters.length)];}
-function getLength (){return lengthCase}
 
-function passwordCriteria(){
-      const lengthCase = window.prompt("How many characters do you want in your password? Choose from 8-128 characters.")
-      if (lengthCase < 8 || lengthCase > 128){ window.alert("Your generated password needs to have a length of 8-128 characters.");
-      }
-      else { const lengthcase = length;
-      }
-
-      const upperCase = window.confirm("Do you want uppercase letters in your generated password?");
-      const lowerCase = window.confirm("Do you want lowercase letters in your generated password?");
-      const numberCase = window.confirm("Do you want numbers in your generated password?");
-      const symbolCase = window.confirm("Do you want special letters in your generated password?");
-
-      //requestedCriteria is an array that lists the following as true or false and applies the rules to make a new password. 
-    const requestedCriteria = {
-      length: getLength,
-      lower: lowerCase,
-      upper: upperCase,
-      number: numberCase,
-      symbol: symbolCase
-    };
-} 
-    
-  function test(){
-  const Criteria = passwordCriteria()
-  console.log(criteria)
-}console.log(test());
 
 function generatePassword(){
-  requestedCriteria;
+  //this will determine the length of the password.
+  const lengthCase = window.prompt("How many characters do you want in your password? Choose from 8-128 characters.")
+  if (lengthCase < 8 || lengthCase > 128 || null){ window.alert("Your generated password needs to have a length of 8-128 characters."); 
+  generatePassword();
+  } 
+  const lc = window.confirm("Do you want lowercase letters in your generated password?");
+  const uc = window.confirm("Do you want uppercase letters in your generated password?");
+  const nc = window.confirm("Do you want numbers in your generated password?");
+  const sc = window.confirm("Do you want special letters in your generated password?");
+
+  //requestedCriteria is an array that lists the following as true or false and applies the rules to make a new password. 
+  const requestedCriteria = {
+    length: lengthCase,
+    lower: lc,
+    upper: uc,
+    number: nc,
+    symbol: sc
+  };
+
+  console.log(requestedCriteria);
+  console.log(getLowerLetter());
+  console.log(getUpperLetter());
+  console.log(getNumber());
+  console.log(getSymbol());
+  console.log(lengthCase);
+
+// make a new array with the criteria values 
+ 
+if (lc) {
 }
 
-
+}
 // Everything below this line is given 
 // _______________________
 
